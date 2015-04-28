28ed0375fb4b9480418ec57ade41fc16f432d38a
Randy McLain <Randoramma@me.com>
2015-04-27 19:26:46 -0700

    adding the gitignore

---
+++ Protected.m
@@ -0,0 +1,9 @@
+//
+//  Protected.m
+//  RM LocationReminder
+//
+//  Created by Randy McLain on 4/27/15.
+//  Copyright (c) 2015 Randy McLain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
