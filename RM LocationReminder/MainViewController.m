5305356ef975adcb25a8206c9e8b810c25661da7
Randy McLain <Randoramma@me.com>
2015-04-28 11:02:01 -0700

    instead I got the 3 buttons working.

--- RM LocationReminder/MainViewController.m
+++ RM LocationReminder/MainViewController.m
@@ -16,18 +16,65 @@
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *myButton2;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *myButton3;
 
+#define METERS_PER_MILE 1609.344
+
+
 @end
 
 @implementation MainViewController
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view, typically from a nib.
+  
 }
+
+- (void)viewWillAppear:(BOOL) animated {
+  CLLocationCoordinate2D seattleLocation;
+  seattleLocation.latitude = 47.60;
+  seattleLocation.longitude = -122.33;
+  
+  MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(seattleLocation, 0.5*METERS_PER_MILE, 0.5*METERS_PER_MILE);
+
+  [_myMainMapView setRegion: viewRegion animated: YES];
+  
+}
+- (IBAction)button1Pressed:(id)sender {
+  CLLocationCoordinate2D appleLocation;
+  appleLocation.latitude = 37.3317;
+  appleLocation.longitude = -122.03033;
+  MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(appleLocation, 0.25*METERS_PER_MILE, 0.25*METERS_PER_MILE);
+  
+  [_myMainMapView setRegion: viewRegion animated: YES];
+}
+
+- (IBAction)button2Pressed:(id)sender {
+  CLLocationCoordinate2D lhcLocation;
+  lhcLocation.latitude = 46.23;
+  lhcLocation.longitude = 6.05;
+  MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(lhcLocation, 3*METERS_PER_MILE, 3*METERS_PER_MILE);
+  
+  [_myMainMapView setRegion: viewRegion animated: YES];
+  
+}
+
+- (IBAction)button3Pressed:(id)sender {
+  CLLocationCoordinate2D piLocation;
+  piLocation.latitude = 25.12;
+  piLocation.longitude = 55.13;
+  MKCoordinateRegion viewRegion = MKCoordinateRegionMakeWithDistance(piLocation, 4*METERS_PER_MILE, 4*METERS_PER_MILE);
+  
+  [_myMainMapView setRegion: viewRegion animated: YES];
+
+}
+
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
   // Dispose of any resources that can be recreated.
 }
+
+
+
+
 
 @end
