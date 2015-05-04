83637143e4dd421cf62535364e8294de5cda88d1
Randy McLain <Randoramma@me.com>
2015-05-04 10:47:15 -0700

    watch working with deprecated code.

--- RM LocationReminder WatchKit Extension/MapDetailController.m
+++ RM LocationReminder WatchKit Extension/MapDetailController.m
@@ -9,8 +9,28 @@
 #import "MapDetailController.h"
 #import <WatchKit/WatchKit.h>
 
+@interface MapDetailController ()
+
+@property (weak, nonatomic) IBOutlet WKInterfaceMap *mapDetail;
+@property (weak, nonatomic) IBOutlet WKInterfaceLabel *mapLabel;
+
+@end
+
 @implementation MapDetailController: WKInterfaceController
 
+-(void)awakeWithContext:(id)context {
+  CLRegion *theRegion = context;
+  
+  [self.mapLabel setText:theRegion.identifier];
+  
+  
+  MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(theRegion.center, 500.0, 500.0);
 
+  [self.mapDetail setRegion:region];
+}
+
+-(void)willActivate {
+
+}
 
 @end
